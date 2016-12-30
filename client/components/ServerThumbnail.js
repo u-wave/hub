3a62b19b0b47516cb6388c9b4169edf842b9d25d
René Kooi <renee@kooi.me>
2016-12-30 17:37:41 +0100

    client: Add Material-UI server cards.

--- client/components/ServerThumbnail.js
+++ client/components/ServerThumbnail.js
@@ -1,38 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Card, CardHeader, CardTitle, CardMedia } from 'material-ui/Card';
 
 export default ({
-  server
+  server,
+  media
 }) => (
-  <div className="thumb">
-    <header>
-      {server.name}
-    </header>
-    {server.booth && server.booth.media && (
-      <div className="media">
-        <img
-          src={server.booth.media.media.thumbnail}
-          title={`${server.booth.media.artist} - ${server.booth.media.title}`}
-        />
-      </div>
+  <Card className="thumb">
+    <CardHeader title={server.name} />
+    {media && (
+      <CardMedia
+        overlay={(
+          <CardTitle
+            title={media.title}
+            subtitle={media.artist}
+          />
+        )}
+      >
+        <img src={media.media.thumbnail} />
+      </CardMedia>
     )}
     <style jsx>{`
       .thumb {
-        width: 300px;
-      }
-      header: {
-        font-size: 125%;
-        border-bottom: 1px solid red;
-      }
-      .media {
-        width: 100%;
-        padding-bottom: 56.25%;
-        position: relative;
-        overflow: hidden;
-      }
-      img {
-        position: absolute;
-        width: 100%;
+        width: 360px;
       }
     `}</style>
-  </div>
+  </Card>
 );
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import fetch from 'isomorphic-fetch';
+import AppBar from 'material-ui/AppBar';
+import ThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 
 import ServerThumbnail from '../components/ServerThumbnail';
 
-export default class ServerListing extends React.Component {
+const ServerListing = ({
+  servers
+}) => (
+  <div className="servers">
+    {servers.map((server) => (
+      <ServerThumbnail
+        key={server.url}
+        server={server}
+        media={server.booth && server.booth.media}
+      />
+    ))}
+    <style jsx>{`
+      .servers {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+      }
+    `}</style>
+  </div>
+);
+
+export default class App extends React.Component {
   static async getInitialProps() {
     const response = await fetch('http://localhost:6451/')
     const state = await response.json()
@@ -12,21 +37,40 @@ export default class ServerListing extends React.Component {
 
   render() {
     return (
-      <div className="servers">
-        {this.props.servers.map((server) => (
-          <ServerThumbnail
-            key={server.url}
-            server={server}
-          />
-        ))}
-        <style jsx>{`
-          .servers {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-around;
-          }
-        `}</style>
-      </div>
+      <ThemeProvider muiTheme={getMuiTheme(darkTheme)}>
+        <div className="app">
+          <AppBar title="üWave" />
+
+          <div className="main">
+            <ServerListing servers={this.props.servers} />
+          </div>
+
+          <style jsx global>{`
+            body {
+              margin: 0;
+            }
+          `}</style>
+          <style jsx>{`
+            .app {
+              background: #303030;
+              color: #fff;
+              position: absolute;
+              height: 100%;
+              width: 100%;
+            }
+
+            .main {
+              position: absolute;
+              padding-top: 20px;
+              top: 64px;
+              bottom: 0;
+              right: 0;
+              left: 0;
+              overflow-y: auto;
+            }
+          `}</style>
+        </div>
+      </ThemeProvider>
     );
   }
 }
