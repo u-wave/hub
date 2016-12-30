f96316eaa85b5f1d8d27fc9b290ada543224e499
René Kooi <renee@kooi.me>
2016-12-30 18:38:03 +0100

    client: Add server link to thumbnails.

--- client/components/ServerThumbnail.js
+++ client/components/ServerThumbnail.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { Card, CardHeader, CardTitle, CardMedia } from 'material-ui/Card';
+import { Card, CardHeader, CardMedia, CardTitle } from 'material-ui/Card';
 
 const ServerThumbnail = ({
   server,
   media
 }) => (
   <Card className="thumb">
-    <CardHeader title={server.name} />
-    {media && (
-      <CardMedia
-        overlay={(
-          <CardTitle
-            title={media.title}
-            subtitle={media.artist}
-          />
-        )}
-      >
-        <img src={media.media.thumbnail} />
-      </CardMedia>
-    )}
+    <a href={server.url}>
+      <CardHeader title={server.name} subtitle={server.description} />
+      {media && (
+        <CardMedia
+          overlay={(
+            <CardTitle
+              title={media.title}
+              subtitle={media.artist}
+            />
+          )}
+        >
+          <img src={media.media.thumbnail} />
+        </CardMedia>
+      )}
+    </a>
     <style jsx>{`
       .thumb {
         width: 360px;
