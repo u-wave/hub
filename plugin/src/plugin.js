2efc0f2a2c6c84c0810b78a68e484f33204fc653
René Kooi <renee@kooi.me>
2017-01-02 00:20:11 +0100

    plugin: Pull announcement data builder into own function.

--- plugin/src/plugin.js
+++ plugin/src/plugin.js
@@ -5,6 +5,38 @@ function stripSlashes(url) {
   return url.replace(/\/+$/, '');
 }
 
+async function getAnnounceData(uw, options) {
+  // TODO add something to üWave Core so we don't have to manually populate
+  // the relationships.
+  const entry = await uw.booth.getCurrentEntry();
+  if (entry) {
+    entry.populate('user media.media');
+    await entry.execPopulate();
+  }
+
+  return {
+    name: options.name,
+    subtitle: options.subtitle,
+    description: options.description || null,
+
+    booth: entry ? {
+      media: {
+        artist: entry.media.artist,
+        title: entry.media.title,
+        thumbnail: entry.media.media.thumbnail,
+      },
+      dj: entry.user ? {
+        username: entry.user.username,
+      } : null,
+    } : null,
+
+    url,
+    // Derive URLs if not given.
+    apiUrl: options.apiUrl || `${url}/v1`,
+    socketUrl: options.socketUrl || `${url.replace(/^http/, 'ws')}/`,
+  };
+}
+
 module.exports = function announcePlugin(options) {
   const hubHost = options.hub || 'https://u-wave-announce.now.sh';
   const url = stripSlashes(options.url);
@@ -13,37 +45,11 @@ module.exports = function announcePlugin(options) {
 
   return (uw) => {
     async function announce() {
-      // TODO add something to üWave Core so we don't have to manually populate
-      // the relationships.
-      const entry = await uw.booth.getCurrentEntry();
-      if (entry) {
-        entry.populate('user media.media');
-        await entry.execPopulate();
-      }
+      const announcement = await getAnnounceData(uw, options);
 
       await got.post(announceUrl, {
         headers: { 'content-type': 'application/json' },
-        body: JSON.stringify({
-          name: options.name,
-          subtitle: options.subtitle,
-          description: options.description || null,
-
-          booth: entry ? {
-            media: {
-              artist: entry.media.artist,
-              title: entry.media.title,
-              thumbnail: entry.media.media.thumbnail,
-            },
-            dj: entry.user ? {
-              username: entry.user.username,
-            } : null,
-          } : null,
-
-          url,
-          // Derive URLs if not given.
-          apiUrl: options.apiUrl || `${url}/v1`,
-          socketUrl: options.socketUrl || `${url.replace(/^http/, 'ws')}/`,
-        }),
+        body: JSON.stringify(announcement),
       });
     }
 
