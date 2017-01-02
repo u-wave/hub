0a911dd026f30b178f76a9ef04b9647ac3cd3897
René Kooi <renee@kooi.me>
2017-01-02 22:13:55 +0100

    client: Remove unused vars from material-ui theme.

--- client/muiTheme.js
+++ client/muiTheme.js
@@ -5,13 +5,6 @@ import * as Spacing from 'material-ui/styles/spacing';
 export default {
   spacing: Spacing,
   fontFamily: 'Open Sans, sans-serif',
-  rankColors: {
-    admin: '#ff3b74',
-    manager: '#05daa5',
-    moderator: '#00b3dc',
-    special: '#fc911d',
-    default: ''
-  },
   palette: {
     primary1Color: '#9d2053',
     primary2Color: '#b20062',
@@ -20,14 +13,9 @@ export default {
     accent2Color: Colors.grey100,
     accent3Color: Colors.grey500,
     textColor: Colors.white,
-    alternateTextColor: Colors.darkBlack,
+    alternateTextColor: Colors.lightWhite,
     canvasColor: '#303030',
     borderColor: ColorManipulator.fade(Colors.fullWhite, 0.3),
-    disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3),
-
-    notifications: {
-      errorBackgroundColor: '#9d202f',
-      errorTextColor: Colors.white
-    }
-  }
+    disabledColor: ColorManipulator.fade(Colors.darkWhite, 0.3),
+  },
 };
