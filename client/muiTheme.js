77360c302192f0cec4aaed812f695a9a0ee9939f
Renée Kooi <renee@kooi.me>
2017-05-26 16:05:53 +0200

    client: Upgrade dependencies.

--- client/muiTheme.js
+++ client/muiTheme.js
@@ -1,9 +1,11 @@
 import createPalette from 'material-ui/styles/palette';
 import createTheme from 'material-ui/styles/theme';
+import { pink } from 'material-ui/styles/colors';
 
 export default function muiTheme() {
   const palette = createPalette({
     primary: {
+      ...pink,
       500: '#9d2053',
       700: '#b20062',
       contrastDefaultColor: 'light'
