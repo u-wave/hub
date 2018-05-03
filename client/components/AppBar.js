43f82aa953c6386ac608db9973bc5b5619d0f6a7
Renée Kooi <renee@kooi.me>
2018-05-03 10:05:09 +0200

    client: Use same AppBar as u-wave.net.

--- client/components/AppBar.js
+++ client/components/AppBar.js
@@ -1,46 +1,48 @@
 import React from 'react'
 import Link from 'next/link'
-import MuiAppBar from 'material-ui/AppBar'
+import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Button from 'material-ui/Button'
 import { withStyles } from 'material-ui/styles'
 
-const enhance = withStyles({
-  gutter: {
-    flex: 0.1
-  },
+const LOGO_HEIGHT = 114
+const LOGO_SQUARE_WIDTH = 104
+
+const enhance = withStyles(theme => ({
   logo: {
-    flex: 1,
-    boxSizing: 'border-box',
-    padding: '7px 0',
-    height: 64,
-    textAlign: 'center'
+    height: 48,
+    marginRight: 48,
+    [theme.breakpoints.down('md')]: {
+      width: `${LOGO_SQUARE_WIDTH * 48 / LOGO_HEIGHT}px`,
+      overflow: 'hidden',
+      marginRight: 12,
+    },
   },
   logoImg: {
-    maxHeight: '100%'
-  }
-}, { name: 'AppBar' })
+    maxHeight: '100%',
+  },
+}));
 
-const AppBar = ({ classes }) => (
-  <span>
-    <MuiAppBar>
-      <Toolbar>
-        <div className={classes.gutter} />
-        <Link href='/'>
-          <div className={classes.logo}>
-            <img className={classes.logoImg} src='/static/logo-white.png' />
-          </div>
-        </Link>
-        <Button
-          contrast
-          className={classes.gutter}
-          href='https://u-wave.net'
-        >
-          About
+const Header = ({ classes }) => (
+  <AppBar elevation={0}>
+    <Toolbar>
+      <Link href="https://u-wave.net/">
+        <a className={classes.logo}>
+          <img src="/static/logo-white.png" className={classes.logoImg} />
+        </a>
+      </Link>
+      <Link href="/" passHref>
+        <Button variant="flat">
+          Join
         </Button>
-      </Toolbar>
-    </MuiAppBar>
-  </span>
-)
+      </Link>
+      <Link href="https://u-wave.net/install" passHref>
+        <Button variant="flat">
+          Install
+        </Button>
+      </Link>
+    </Toolbar>
+  </AppBar>
+);
 
-export default enhance(AppBar)
+export default enhance(Header)
