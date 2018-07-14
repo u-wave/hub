27ebeab658ed72c4f9a49da38eb7197e091b760d
Renée Kooi <renee@kooi.me>
2018-07-14 14:29:33 +0200

    Use lerna. (#9)
    * Use lerna.
    
    * Configure standard differently

--- plugin/rollup.config.js
+++ plugin/rollup.config.js
@@ -1,10 +1,10 @@
-import json from 'rollup-plugin-json';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import isBuiltinModule from 'is-builtin-module';
+import json from 'rollup-plugin-json'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import isBuiltinModule from 'is-builtin-module'
 
-const pkg = require('./package.json');
+const pkg = require('./package.json')
 
-const external = Object.keys(pkg.dependencies);
+const external = Object.keys(pkg.dependencies)
 
 export default {
   input: 'src/plugin.js',
@@ -12,15 +12,15 @@ export default {
     file: pkg.main,
     exports: 'default',
     format: 'cjs',
-    sourcemap: true,
+    sourcemap: true
   }, {
     file: pkg.module,
     format: 'es',
-    sourcemap: true,
+    sourcemap: true
   }],
   external: id => isBuiltinModule(id) || external.some(m => id.split('/')[0] === m),
   plugins: [
     json(),
-    nodeResolve(),
-  ],
-};
+    nodeResolve()
+  ]
+}
