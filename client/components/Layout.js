7c69fa866142f1d8ba35a7c4893f0dc57eb2542d
Renée Kooi <renee@kooi.me>
2019-12-04 14:42:46 +0100

    client: put everything below MuiThemeProvider

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,50 +1,39 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Head from 'next/head'
 import Typography from '@material-ui/core/Typography'
-import { MuiThemeProvider, withStyles } from '@material-ui/core/styles'
-import compose from 'recompose/compose'
-import withContext from 'recompose/withContext'
-import isMobile from 'is-mobile'
+import { MuiThemeProvider, makeStyles } from '@material-ui/core/styles'
 import theme from '../muiTheme'
 import AppBar from './AppBar'
 
-const enhance = compose(
-  withContext({
-    isMobile: PropTypes.bool
-  }, ({ userAgent }) => ({
-    isMobile: isMobile({ ua: userAgent })
-  })),
-  withStyles({
-    app: {
-      background: '#1b1b1b',
-      color: '#fff',
-      position: 'absolute',
-      height: '100%',
-      width: '100%'
-    },
-    main: {
-      position: 'absolute',
-      paddingTop: 20,
-      top: 64,
-      bottom: 0,
-      right: 0,
-      left: 0,
-      overflowY: 'auto'
-    },
-    footer: {
-      fontFamily: 'monospace',
-      textAlign: 'center',
-      marginTop: 50,
-      color: '#777'
-    },
-    src: {
-      textDecoration: 'none',
-      color: '#aaa',
-      borderBottom: '1px solid #aaa'
-    }
-  }, { name: 'Layout' })
-)
+const useStyles = makeStyles({
+  app: {
+    background: '#1b1b1b',
+    color: '#fff',
+    position: 'absolute',
+    height: '100%',
+    width: '100%'
+  },
+  main: {
+    position: 'absolute',
+    paddingTop: 20,
+    top: 64,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    overflowY: 'auto'
+  },
+  footer: {
+    fontFamily: 'monospace',
+    textAlign: 'center',
+    marginTop: 50,
+    color: '#777'
+  },
+  src: {
+    textDecoration: 'none',
+    color: '#aaa',
+    borderBottom: '1px solid #aaa'
+  }
+}, { name: 'Layout' })
 
 const globalCss = `
   body {
@@ -53,11 +42,10 @@ const globalCss = `
   }
 `
 
-const Layout = ({
-  classes,
-  children
-}) => (
-  <MuiThemeProvider theme={theme}>
+function LayoutElements ({ children }) {
+  const classes = useStyles()
+
+  return (
     <div className={classes.app}>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
@@ -76,7 +64,17 @@ const Layout = ({
         </footer>
       </main>
     </div>
-  </MuiThemeProvider>
-)
+  )
+}
 
-export default enhance(Layout)
+function Layout ({ children }) {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <LayoutElements>
+        {children}
+      </LayoutElements>
+    </MuiThemeProvider>
+  )
+}
+
+export default Layout
