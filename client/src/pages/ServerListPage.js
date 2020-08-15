24c952ac9e7fab681924538d9a2e3383ec3067b2
Renée Kooi <renee@kooi.me>
2020-08-15 14:16:50 +0200

    client: use `react-server-list.Container`

--- client/src/pages/ServerListPage.js
+++ client/src/pages/ServerListPage.js
@@ -1,68 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { loadServers, announceEvents, ServerList } from '@u-wave/react-server-list';
+import { Container as ServerList } from '@u-wave/react-server-list';
 import '@u-wave/react-server-list/dist/u-wave-react-server-list.css';
 import Layout from '../components/Layout';
 import Loading from '../components/Loading';
 
 const HUB_SERVER = process.env.HUB_SERVER;
 
-function addServer(list, update) {
-  const servers = list.map((server) => (server.publicKey === update.publicKey ? update : server));
-  if (servers.indexOf(update) === -1) {
-    servers.unshift(update);
-  }
-  return servers;
+function ServerListPage() {
+  return (
+    <Layout>
+      <ServerList hub={HUB_SERVER} />
+    </Layout>
+  );
 }
 
-export default class ServerListPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      servers: null,
-    };
-
-    this.update = this.update.bind(this);
-    this.handleUpdate = this.handleUpdate.bind(this);
-  }
-
-  componentDidMount() {
-    const { servers } = this.state;
-
-    if (!servers) {
-      this.update();
-    }
-    this.events = announceEvents(HUB_SERVER, this.handleUpdate);
-  }
-
-  componentWillUnmount() {
-    this.events.remove();
-  }
-
-  async update() {
-    this.setState({
-      servers: await loadServers(HUB_SERVER),
-    });
-  }
-
-  handleUpdate(update) {
-    this.setState(({ servers }) => ({
-      servers: addServer(servers, update),
-    }));
-  }
-
-  render() {
-    const { servers } = this.state;
-
-    return (
-      <Layout>
-        {servers == null ? (
-          <Loading message="Loading available servers..." />
-        ) : (
-          <ServerList servers={servers} />
-        )}
-      </Layout>
-    );
-  }
-}
+export default ServerListPage;
