def060a5503fe3454437e952e378f8512ece7f94
Renée Kooi <renee@kooi.me>
2021-03-05 17:34:13 +0100

    client: use react strict mode

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,10 @@
 exports.target = 'serverless';
-
-exports.exportPathMap = () => ({
-  '/': { page: '/' },
-});
+exports.reactStrictMode = true;
 
 exports.env = {
   HUB_SERVER: process.env.HUB_SERVER || 'https://announce.u-wave.net',
 };
+
+exports.exportPathMap = () => ({
+  '/': { page: '/' },
+});
