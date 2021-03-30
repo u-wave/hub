e24c11be14f549b63b536875d0e4d89eca471501
Renée Kooi <renee@kooi.me>
2021-03-30 13:06:05 +0200

    client: use webpack 5

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 exports.target = 'serverless';
 exports.reactStrictMode = true;
 
+exports.future = {
+  webpack5: true,
+};
+
 exports.env = {
   HUB_SERVER: process.env.HUB_SERVER || 'https://announce.u-wave.net',
 };
