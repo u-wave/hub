a69b95db64613d654dab7b5776a051dc0c8600f6
Renée Kooi <renee@kooi.me>
2021-07-06 15:33:13 +0200

    plugin: add crash test

---
+++ plugin/test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+assert.doesNotThrow(() => {
+  // Just to make sure it does not crash :)
+  // eslint-disable-next-line global-require
+  require('./src/plugin');
+});
