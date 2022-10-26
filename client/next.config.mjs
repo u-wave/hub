d992d83235d01fb59c319e2c4952301242c7d065
Renée Kooi <renee@kooi.me>
2022-10-26 10:19:12 +0200

    client: update next.js

---
+++ client/next.config.mjs
@@ -0,0 +1,9 @@
+export const reactStrictMode = true;
+
+export const env = {
+  HUB_SERVER: process.env.HUB_SERVER || 'https://announce.u-wave.net',
+};
+
+export const exportPathMap = () => ({
+  '/': { page: '/' },
+});
