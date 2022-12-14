f3cc9837a5c9575eb79c7ecd01be5cd9ed9a1342
Renée Kooi <renee@kooi.me>
2022-12-14 20:35:48 +0100

    remove pages/

--- client/pages/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from '@u-wave/react-server-list';
-
-const { VITE_HUB_SERVER } = import.meta.env;
-
-function App() {
-  return <Container hub={VITE_HUB_SERVER} />;
-}
-
-export default App;
