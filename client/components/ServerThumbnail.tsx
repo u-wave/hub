51f7aab860561575c998bf421c6ef03972b72a68
Renée Kooi <renee@kooi.me>
2024-08-25 10:30:29 +0200

    no mui svgicon

--- client/components/ServerThumbnail.tsx
+++ client/components/ServerThumbnail.tsx
@@ -1,28 +1,18 @@
 import { useState } from 'react';
+import { mdiAlert, mdiMenu } from '@mdi/js';
+import Icon from '@mdi/react';
 import IconButton from '@mui/material/IconButton';
-import SvgIcon from '@mui/material/SvgIcon';
 import { intlFormatDistance } from 'date-fns';
 import DescriptionDialog from './DescriptionDialog';
 import CurrentMedia from './CurrentMedia';
 import * as styles from './ServerThumbnail.module.css';
 import type { Media, Server } from '../utils/hub';
 
-const mdiAlert = 'M13 14H11V9H13M13 18H11V16H13M1 21H23L12 2L1 21Z';
-const mdiMenu = 'M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z';
-
 const downTimeout = 600_000; // 10 minutes
 
 function WarningIcon() {
   return (
-    <SvgIcon
-      style={{
-        height: 16,
-        width: 16,
-        verticalAlign: 'sub',
-      }}
-    >
-      <path d={mdiAlert} />
-    </SvgIcon>
+    <Icon size={1} style={{ verticalAlign: 'sub' }} path={mdiAlert} />
   );
 }
 
@@ -72,9 +62,7 @@ function ServerThumbnail({ server, media }: ServerThumbnailProps) {
               setDescriptionOpen(true);
             }}
           >
-            <SvgIcon>
-              <path d={mdiMenu} />
-            </SvgIcon>
+            <Icon size={1} path={mdiMenu} />
           </IconButton>
         )}
       </div>
